4bb56c8317e7a16d2997a5cf3774621fb45ef13d
Ashutosh A <96434947+Ashutosh-Ardu@users.noreply.github.com>
2024-03-18 23:22:05 +0530

    Commit 1
    Init Commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { UsersService } from '../../services/users.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  user = this.usersService.currentUserProfile$;
+
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit(): void {}
+}
